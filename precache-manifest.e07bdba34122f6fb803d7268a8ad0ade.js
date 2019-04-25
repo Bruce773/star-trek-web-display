dd2f14071e342fde108543fb413796540356d057
Bruce <bruce@yogainternational.com>
2019-04-24 21:44:58 -0400

    Updates

---
+++ precache-manifest.e07bdba34122f6fb803d7268a8ad0ade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea2bdf4c6746b6463201",
+    "url": "/star-trek-web-display/static/js/runtime~main.6d3390bf.js"
+  },
+  {
+    "revision": "29d4ae75e2373324b871",
+    "url": "/star-trek-web-display/static/js/main.ddfc9878.chunk.js"
+  },
+  {
+    "revision": "119e5180dfffef53cc53",
+    "url": "/star-trek-web-display/static/js/2.e93d2e2b.chunk.js"
+  },
+  {
+    "revision": "29d4ae75e2373324b871",
+    "url": "/star-trek-web-display/static/css/main.635d1ba7.chunk.css"
+  },
+  {
+    "revision": "eff4ae10aef464615a605318578bbfb3",
+    "url": "/star-trek-web-display/index.html"
+  }
+];
